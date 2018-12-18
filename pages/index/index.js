ff305995cf26a2cb66927199f941194724ef5bb3
zhang0135789 <zz634682577>
2018-12-18 14:39:47 +0800

    init

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,56 @@
-//index.js
+const util = require('../../utils/util.js');
+const api = require('../../config/api.js');
+const user = require('../../services/user.js');
+
 //获取应用实例
 const app = getApp()
-
 Page({
   data: {
-    motto: '欢迎来到甲乙丙科技',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    newGoods: [],
+    hotGoods: [],
+    topics: [],
+    brands: [],
+    floorGoods: [],
+    banner: [],
+    channel: []
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
+  onShareAppMessage: function () {
+    return {
+      title: 'NideShop',
+      desc: '甲乙丙科技',
+      path: '/pages/index/index'
     }
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
+
+  getIndexData: function () {
+    let that = this;
+    util.request(api.IndexUrl).then(function (res) {
+      if (res.errno === 0) {
+        that.setData({
+          newGoods: res.data.newGoodsList,
+          hotGoods: res.data.hotGoodsList,
+          topics: res.data.topicList,
+          brand: res.data.brandList,
+          floorGoods: res.data.categoryList,
+          banner: res.data.banner,
+          channel: res.data.channel
+        });
+      }
+    });
+  },
+  onLoad: function (options) {
+    this.getIndexData();
+  },
+  onReady: function () {
+    // 页面渲染完成
+  },
+  onShow: function () {
+    // 页面显示
+  },
+  onHide: function () {
+    // 页面隐藏
+  },
+  onUnload: function () {
+    // 页面关闭
+  },
 })
